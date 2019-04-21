12f43ce12a736d76aa9ada16bc6a7cb0d68d96ca
Nnamdi Ugwuoke <ugwuokebosco@gmail.com>
2019-04-21 17:12:10 +0400

    Initial Commit

---
+++ RTA/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RTA
+//
+//  Created by Nnamdi Ugwuoke on 4/21/19.
+//  Copyright © 2019 Manipal University Dubai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
